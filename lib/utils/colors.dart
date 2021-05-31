26508705699eae14337e949800e86391d93998ca
cjmazzarri <cjmazzarri@gmail.com>
2021-05-30 19:30:57 -0500

    💄Added color code for font color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color greenColor = Color(0xff30B18B);
 Color purpleColor = Color(0xff8377d1);
 Color backgroundColor = Color(0xffE9EBF8);
+Color greyColor = Color(0x378C8C8C);
