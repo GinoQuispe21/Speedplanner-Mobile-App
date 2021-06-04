e586c6d44313a1720fce3fa5f2fd696d18c74605
cjmazzarri <cjmazzarri@gmail.com>
2021-06-03 20:51:15 -0500

    ✨Added color for Text Field fonts

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 Color greenColor = Color(0xff30B18B);
 Color purpleColor = Color(0xff8377d1);
 Color backgroundColor = Color(0xffE9EBF8);
-Color greyColor = Color(0x378C8C8C);
+Color greyColor = Color(0xff161616);
+Color textFieldColor = Color(0xff707070);
