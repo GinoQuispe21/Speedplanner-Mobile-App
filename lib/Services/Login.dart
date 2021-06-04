3d7be130655937dcebdc1482d343e0fec9ed0ea2
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-06-04 00:41:38 -0500

    Fixed login bug :ambulance:

--- lib/Services/Login.dart
+++ lib/Services/Login.dart
@@ -7,12 +7,12 @@ var urlLogin = Uri.parse('https://speedplanner-mobile.herokuapp.com/login');
 
 class Token {
   String token;
-  Token({this.token});
+  Token({required this.token});
 }
 
 class LoginService {
   Future<Token> login(username, password) async {
-    final userToken = Token();
+    final userToken = Token(token: '');
     http.Response token = await http.post(
       urlLogin,
       body: jsonEncode(<String, String>{
@@ -22,7 +22,7 @@ class LoginService {
     );
     if (token.statusCode == 200) {
       Map<String, String> headers = token.headers;
-      userToken.token = headers['authorization'];
+      userToken.token = headers['authorization'].toString();
     }
     return userToken;
   }
