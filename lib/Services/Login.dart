c097e378c9803066dd8429e9490e28d8cd68f5df
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-06-08 17:13:15 -0500

    Added attributes to return in login service :zap:

--- lib/Services/Login.dart
+++ lib/Services/Login.dart
@@ -1,16 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:speedplanner/models/TokenUserData.dart';
 
 var urlLogin = Uri.parse('https://speedplanner-mobile.herokuapp.com/login');
 
-class Token {
-  String token;
-  Token({required this.token});
-}
-
 class LoginService {
-  Future<Token> login(username, password) async {
-    final userToken = Token(token: '');
+  Future<TokenUserData> login(username, password) async {
+    final tokenUserData = TokenUserData();
+    // * Validation login
     http.Response token = await http.post(
       urlLogin,
       body: jsonEncode(<String, String>{
@@ -20,8 +18,20 @@ class LoginService {
     );
     if (token.statusCode == 200) {
       Map<String, String> headers = token.headers;
-      userToken.token = headers['authorization'].toString();
+      tokenUserData.token = headers['authorization'].toString();
+      print('el token es: ${tokenUserData.token}');
+      // * Get user data by username using token authentication
+      http.Response userResponse = await http.get(
+          Uri.parse(
+              'https://speedplanner-mobile.herokuapp.com/api/usersUsername/$username'),
+          headers: {HttpHeaders.authorizationHeader: tokenUserData.token});
+      Map userdata = jsonDecode(userResponse.body);
+      tokenUserData.id = userdata['id'];
+      tokenUserData.username = userdata['username'];
+      tokenUserData.email = userdata['email'];
+      tokenUserData.password = userdata['password'];
+      tokenUserData.role = userdata['role'];
     }
-    return userToken;
+    return tokenUserData;
   }
 }
