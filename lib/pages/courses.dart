c14ec1d0ea1e165d90aa0f62c9befcc5d3f82d4f
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-06-08 17:14:52 -0500

    Little changes in courses page :zap:

--- lib/pages/courses.dart
+++ lib/pages/courses.dart
@@ -9,30 +9,32 @@ class _CoursesState extends State<Courses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        /*floatingActionButton: FloatingActionButton(
-          backgroundColor: Color(0x00000000),
-          elevation: 0,
-          onPressed: () {
-            Navigator.pushNamed(context, '/addCourse');
-          },
-          child: const Icon(
-            Icons.add_circle_outline_sharp,
-            color: Color(0xff8377D1),
-            size: 50.0,
-          ),
-        ),*/
-        body: Container(
-      decoration: BoxDecoration(color: Color(0xffE9EBF8)),
-      child: Center(
-        child: Text(
-          'Todavía no se han creado Cursos',
-          style: TextStyle(
-              color: Color(0xff9C9DA6),
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic),
+      floatingActionButton: FloatingActionButton(
+        heroTag: "btn1",
+        backgroundColor: Color(0x00000000),
+        elevation: 0,
+        onPressed: () {
+          Navigator.pushNamed(context, '/addCourse');
+        },
+        child: const Icon(
+          Icons.add_circle_outline_sharp,
+          color: Color(0xff8377D1),
+          size: 50.0,
         ),
       ),
-    ));
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xffE9EBF8)),
+        child: Center(
+          child: Text(
+            'Todavía no se han creado Cursos',
+            style: TextStyle(
+                color: Color(0xff9C9DA6),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+    );
   }
 }
