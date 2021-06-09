547d119414d5dc5f28234de737642b447f599b2d
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-06-09 00:58:00 -0500

    Improved ui in Courses :lipstick:

--- lib/pages/courses.dart
+++ lib/pages/courses.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:speedplanner/models/Course.dart';
 import 'package:speedplanner/services/GetAllCourses.dart';
+import 'package:speedplanner/utils/dateFooter.dart';
 
 class Courses extends StatefulWidget {
   final int id;
@@ -15,6 +17,12 @@ class Courses extends StatefulWidget {
 class _CoursesState extends State<Courses> {
   GetAllCoursesByUserIdService getCourses = new GetAllCoursesByUserIdService();
   List<Course> listCourse = [];
+  String formatter = '';
+  void getCurrentDate() async {
+    DateTime now = new DateTime.now();
+    formatter = DateFormat('yMMMd').format(now);
+    print(formatter);
+  }
 
   void getCourse() async {
     await getCourses.getAllCoursesByUserId(widget.id, widget.token);
@@ -26,6 +34,7 @@ class _CoursesState extends State<Courses> {
   @override
   void initState() {
     super.initState();
+    getCurrentDate();
     print('El id en Course es : ${widget.id}');
     print('El token en Course es : ${widget.token}');
     getCourse();
@@ -33,8 +42,11 @@ class _CoursesState extends State<Courses> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 40),
+        child: FloatingActionButton(
           heroTag: "btn1",
           backgroundColor: Color(0x00000000),
           elevation: 0,
@@ -42,37 +54,80 @@ class _CoursesState extends State<Courses> {
             Navigator.pushNamed(context, '/addCourse');
           },
           child: const Icon(
-            Icons.add_circle_outline_sharp,
+            Icons.add_circle,
             color: Color(0xff8377D1),
             size: 50.0,
           ),
         ),
-        body: listCourse.isEmpty
-            ? notCourses()
-            : Container(
-                decoration: BoxDecoration(color: Color(0xffE9EBF8)),
-                child: ListView.builder(
-                  itemCount: listCourse.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 1.0, horizontal: 4.0),
-                      child: Card(
-                        child: ListTile(
-                          onTap: () {},
-                          title: Text(
-                            listCourse[index].name,
+      ),
+      body: listCourse.isEmpty
+          ? notCourses()
+          : Container(
+              height: size.height,
+              width: double.infinity,
+              decoration: BoxDecoration(color: Color(0xffE9EBF8)),
+              child: Container(
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                                child: Text(
+                              'Lista de Cursos',
+                              style: TextStyle(fontSize: 18),
+                            )),
                           ),
-                          subtitle: Text(
-                            listCourse[index].description,
+                          Container(
+                            height: size.height / 1.57,
+                            width: double.infinity,
+                            child: ListView.builder(
+                              itemCount: listCourse.length,
+                              itemBuilder: (context, index) {
+                                return cardCourse(listCourse[index]);
+                              },
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    );
-                  },
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: dateFooter(
+                          context: context, currentDate: 'Date: ' + formatter),
+                    )
+                  ],
                 ),
-              ));
+              ),
+            ),
+    );
   }
+}
+
+Widget cardCourse(Course course) {
+  return Container(
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+      child: Card(
+        child: Container(
+          decoration: BoxDecoration(color: Color(0xff80C3B5)),
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('${course.name}'),
+                Text('${course.description}'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
 Widget notCourses() {
