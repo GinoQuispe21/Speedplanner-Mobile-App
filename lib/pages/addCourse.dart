015178c929b1d1fabeecbec60e7d9849f70a4040
Juan Alonso <leyva.calle1403@gmail.com>
2021-06-11 22:36:52 -0500

    timeShow works
    yei

--- lib/pages/addCourse.dart
+++ lib/pages/addCourse.dart
@@ -3,7 +3,6 @@ import 'package:speedplanner/utils/colors.dart';
 import 'package:speedplanner/utils/normalInput.dart';
 import 'package:speedplanner/utils/desInput.dart';
 import 'package:speedplanner/utils/miniInput.dart';
-import 'package:speedplanner/pages/courses.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -17,10 +16,70 @@ class _AddCourseState extends State<AddCourse> {
   TextEditingController descText = TextEditingController();
   TextEditingController emailText = TextEditingController();
 
-  var url = Uri.parse(
+  TextEditingController dayText = TextEditingController();
+  TextEditingController startText = TextEditingController();
+  TextEditingController finishText = TextEditingController();
+
+  var dayArr = [];
+  var startArr = [];
+  var finishArr = [];
+
+  void timeString(TextEditingController day, TextEditingController start,
+      TextEditingController finish) {
+    String dayString, startString, finishString;
+    dayString = day.text;
+    startString = start.text;
+    finishString = finish.text;
+    if (dayString == '' || startString == '' || finishString == '') {
+      print("efe");
+    } else {
+      dayArr.add(dayText.text);
+      startArr.add(startText.text);
+      finishArr.add(finishText.text);
+      print("gotcha");
+    }
+  }
+
+  String timeMessage = '';
+
+  timeShow(message) {
+    String day, start, finish;
+
+    if (dayArr.isEmpty || startArr.isEmpty || finishArr.isEmpty) {
+      message = 'No hay cursos disponibles';
+    } else if (dayArr.length == 1 &&
+        startArr.length == 1 &&
+        finishArr.length == 1) {
+      day = dayArr[0];
+      start = startArr[0];
+      finish = finishArr[0];
+      message = '$day: $start - $finish\n';
+    } else if (dayArr.length == 2 &&
+        startArr.length == 2 &&
+        finishArr.length == 2) {
+      day = dayArr[1];
+      start = startArr[1];
+      finish = finishArr[1];
+      message = '$day: $start - $finish\n'
+          '$day: $start - $finish';
+    } else if (dayArr.length == 3 &&
+        startArr.length == 3 &&
+        finishArr.length == 3) {
+      day = dayArr[2];
+      start = startArr[2];
+      finish = finishArr[2];
+      message = '$day: $start - $finish\n'
+          '$day: $start - $finish';
+    }
+
+    return message;
+  }
+
+  var urlCourses = Uri.parse(
       'https://speedplanner-mobile.herokuapp.com/api/users/1/courses');
+
   Future<http.Response> createCourse(name, description, email) async {
-    http.Response response = await http.post(url,
+    http.Response response = await http.post(urlCourses,
         headers: <String, String>{
           'Content-Type': 'application/json',
           'Authorization':
@@ -33,10 +92,30 @@ class _AddCourseState extends State<AddCourse> {
         }));
     if (response.statusCode == 200) {
       print("Curso creado: $name");
+      /*Map dataCourse = jsonDecode(utf8.decode(response.bodyBytes));
+      int idCourse = dataCourse['id'];
+      http.Response responseTime = await http.post(
+        Uri.parse('https://speedplanner-mobile.herokuapp.com/api/courses/$idCourse/times'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization':
+              'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnaW5vIn0.bCcj99sO-yCeKqTfxBEUMinv8ei5EEsSDZy-mG1tjHaE6Z4Pn9YB7bJCrUOaqp-1pV1vXIBiPcNTY7KFWh12Zw'
+        },
+        body: jsonEncode(<String, String>{
+          'day': name,
+          'startTime': description,
+          'finishTime': email,
+        }));*/
     } else {
       print("Error en la creación");
     }
     return response;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    timeMessage = timeShow(timeMessage);
   }
 
   @override
@@ -142,7 +221,7 @@ class _AddCourseState extends State<AddCourse> {
                         child: Column(
                           children: <Widget>[
                             Text(
-                              'Agregar horario',
+                              'Agregar horario (máx. 3)',
                               textAlign: TextAlign.start,
                               style: TextStyle(
                                   color: Colors.white,
@@ -160,15 +239,18 @@ class _AddCourseState extends State<AddCourse> {
                               direction: Axis.horizontal,
                               spacing: 5.0,
                               children: [
-                                miniInput(),
-                                miniInput(),
-                                miniInput(),
+                                miniInput(controller: dayText),
+                                miniInput(controller: startText),
+                                miniInput(controller: finishText),
                                 FloatingActionButton(
                                   backgroundColor: Color(0x00000000),
                                   elevation: 0,
                                   mini: true,
                                   onPressed: () {
-                                    print("yes");
+                                    timeString(dayText, startText, finishText);
+                                    setState(() {
+                                      timeMessage = timeShow(timeMessage);
+                                    });
                                   },
                                   child: const Icon(
                                     Icons.add_circle_outline_sharp,
@@ -211,7 +293,7 @@ class _AddCourseState extends State<AddCourse> {
                         padding: EdgeInsets.only(top: 10, bottom: 15),
                         child: Container(
                           child: Text(
-                            'No hay horarios para este curso',
+                            timeMessage,
                             textAlign: TextAlign.start,
                             style: TextStyle(
                                 color: Colors.white,
