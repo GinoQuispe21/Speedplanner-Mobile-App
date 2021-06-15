18160ca9251a83de4c5980228a3baeaf8cf03f67
Juan Alonso <leyva.calle1403@gmail.com>
2021-06-14 22:47:43 -0500

    Color picker added
    yes

--- lib/pages/addCourse.dart
+++ lib/pages/addCourse.dart
@@ -3,6 +3,7 @@ import 'package:speedplanner/utils/colors.dart';
 import 'package:speedplanner/utils/normalInput.dart';
 import 'package:speedplanner/utils/desInput.dart';
 import 'package:speedplanner/utils/miniInput.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -40,10 +41,28 @@ class _AddCourseState extends State<AddCourse> {
     }
   }
 
+  // create some values
+  //Color pickerColor = firstChoice;
+  //Color currentColor = firstChoice;
+
+  Color currentColor = Colors.limeAccent;
+  List<Color> currentColors = [Colors.limeAccent, Colors.green];
+
+  void changeColor(Color color) => setState(() => currentColor = color);
+  void changeColors(List<Color> colors) =>
+      setState(() => currentColors = colors);
+
+  // ValueChanged<Color> callback
+  /*void changeColor(Color color) {
+    setState(() => pickerColor = color);
+  }*/
+
   String timeMessage = '';
 
   timeShow(message) {
     String day, start, finish;
+    String day2, start2, finish2;
+    String day3, start3, finish3;
 
     if (dayArr.isEmpty || startArr.isEmpty || finishArr.isEmpty) {
       message = 'No hay cursos disponibles';
@@ -57,19 +76,29 @@ class _AddCourseState extends State<AddCourse> {
     } else if (dayArr.length == 2 &&
         startArr.length == 2 &&
         finishArr.length == 2) {
-      day = dayArr[1];
-      start = startArr[1];
-      finish = finishArr[1];
+      day = dayArr[0];
+      start = startArr[0];
+      finish = finishArr[0];
+      day2 = dayArr[1];
+      start2 = startArr[1];
+      finish2 = finishArr[1];
       message = '$day: $start - $finish\n'
-          '$day: $start - $finish';
+          '$day2: $start2 - $finish2';
     } else if (dayArr.length == 3 &&
         startArr.length == 3 &&
         finishArr.length == 3) {
-      day = dayArr[2];
-      start = startArr[2];
-      finish = finishArr[2];
+      day = dayArr[0];
+      start = startArr[0];
+      finish = finishArr[0];
+      day2 = dayArr[1];
+      start2 = startArr[1];
+      finish2 = finishArr[1];
+      day3 = dayArr[2];
+      start3 = startArr[2];
+      finish3 = finishArr[2];
       message = '$day: $start - $finish\n'
-          '$day: $start - $finish';
+          '$day2: $start2 - $finish2\n'
+          '$day3: $start3 - $finish3';
     }
 
     return message;
@@ -78,7 +107,7 @@ class _AddCourseState extends State<AddCourse> {
   var urlCourses = Uri.parse(
       'https://speedplanner-mobile.herokuapp.com/api/users/1/courses');
 
-  Future<http.Response> createCourse(name, description, email) async {
+  Future<http.Response> createCourse(name, description, email, color) async {
     http.Response response = await http.post(urlCourses,
         headers: <String, String>{
           'Content-Type': 'application/json',
@@ -89,23 +118,27 @@ class _AddCourseState extends State<AddCourse> {
           'name': name,
           'description': description,
           'email': email,
+          'color': color
         }));
     if (response.statusCode == 200) {
       print("Curso creado: $name");
-      /*Map dataCourse = jsonDecode(utf8.decode(response.bodyBytes));
-      int idCourse = dataCourse['id'];
-      http.Response responseTime = await http.post(
-        Uri.parse('https://speedplanner-mobile.herokuapp.com/api/courses/$idCourse/times'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization':
-              'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnaW5vIn0.bCcj99sO-yCeKqTfxBEUMinv8ei5EEsSDZy-mG1tjHaE6Z4Pn9YB7bJCrUOaqp-1pV1vXIBiPcNTY7KFWh12Zw'
-        },
-        body: jsonEncode(<String, String>{
-          'day': name,
-          'startTime': description,
-          'finishTime': email,
-        }));*/
+      /*for (var i = 0; i <= dayArr.length; i++) {
+        Map dataCourse = jsonDecode(utf8.decode(response.bodyBytes));
+        int idCourse = dataCourse['id'];
+        http.Response responseTime = await http.post(
+            Uri.parse(
+                'https://speedplanner-mobile.herokuapp.com/api/courses/$idCourse/times'),
+            headers: <String, String>{
+              'Content-Type': 'application/json',
+              'Authorization':
+                  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnaW5vIn0.bCcj99sO-yCeKqTfxBEUMinv8ei5EEsSDZy-mG1tjHaE6Z4Pn9YB7bJCrUOaqp-1pV1vXIBiPcNTY7KFWh12Zw'
+            },
+            body: jsonEncode(<String, String>{
+              'day': dayArr[i],
+              'startTime': startArr[i],
+              'finishTime': finishArr[i],
+            }));
+      }*/
     } else {
       print("Error en la creación");
     }
@@ -116,6 +149,7 @@ class _AddCourseState extends State<AddCourse> {
   void initState() {
     super.initState();
     timeMessage = timeShow(timeMessage);
+    changeColor(firstChoice);
   }
 
   @override
@@ -129,7 +163,7 @@ class _AddCourseState extends State<AddCourse> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-              color: firstChoice,
+              color: currentColor,
               child: Container(
                 margin: EdgeInsets.only(left: 35, right: 35),
                 child: Column(
@@ -139,14 +173,51 @@ class _AddCourseState extends State<AddCourse> {
                         padding: EdgeInsets.only(top: 20, bottom: 10),
                         child: Column(
                           children: <Widget>[
-                            Text(
-                              'Crear Curso',
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  fontStyle: FontStyle.italic),
+                            Wrap(
+                              direction: Axis.horizontal,
+                              spacing: 52.0,
+                              children: [
+                                Text(
+                                  'Crear Curso',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text('Colr del Curso'),
+                                          content: SingleChildScrollView(
+                                            child: BlockPicker(
+                                              pickerColor: currentColor,
+                                              onColorChanged: changeColor,
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                  child: const Text('Cambia el color!'),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: currentColor,
+                                    side: BorderSide(
+                                      width: 4.0,
+                                      color: backgroundColor,
+                                    ),
+                                    textStyle: TextStyle(
+                                      color: useWhiteForeground(currentColor)
+                                          ? const Color(0xffffffff)
+                                          : const Color(0xff000000),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         )),
@@ -239,9 +310,11 @@ class _AddCourseState extends State<AddCourse> {
                               direction: Axis.horizontal,
                               spacing: 5.0,
                               children: [
-                                miniInput(controller: dayText),
-                                miniInput(controller: startText),
-                                miniInput(controller: finishText),
+                                miniInput(controller: dayText, hint: "Día"),
+                                miniInput(
+                                    controller: startText, hint: "Hora ini."),
+                                miniInput(
+                                    controller: finishText, hint: "Hora fin."),
                                 FloatingActionButton(
                                   backgroundColor: Color(0x00000000),
                                   elevation: 0,
@@ -251,6 +324,10 @@ class _AddCourseState extends State<AddCourse> {
                                     setState(() {
                                       timeMessage = timeShow(timeMessage);
                                     });
+                                    dayText.clear();
+                                    startText.clear();
+                                    finishText.clear();
+                                    print(currentColor.toString());
                                   },
                                   child: const Icon(
                                     Icons.add_circle_outline_sharp,
@@ -282,7 +359,7 @@ class _AddCourseState extends State<AddCourse> {
               ),
             ),
             Container(
-              color: firstChoice,
+              color: currentColor,
               width: double.infinity,
               child: Container(
                 margin: EdgeInsets.only(left: 35, right: 35, bottom: 20),
@@ -307,18 +384,18 @@ class _AddCourseState extends State<AddCourse> {
                         elevation: 0,
                         onPressed: () {
                           // Add your onPressed code here!
-                          createCourse(
-                              nameText.text, descText.text, emailText.text);
+                          createCourse(nameText.text, descText.text,
+                              emailText.text, currentColor.toString());
                         },
                         label: const Text('Crear Curso'),
                         backgroundColor: backgroundColor,
-                        foregroundColor: firstChoice,
+                        foregroundColor: currentColor,
                       ),
                     ]),
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 45),
+              padding: EdgeInsets.only(top: 35),
               child: Container(
                   color: dateBG,
                   width: double.infinity,
