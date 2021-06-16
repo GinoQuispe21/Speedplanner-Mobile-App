738caf65917992f38c4106d07e8ac72aa236c758
cjmazzarri <cjmazzarri@gmail.com>
2021-06-16 01:00:28 -0500

    ✨Added dynamic routing to Profile

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
       Courses(id: id, token: token),
       Tasks(),
       Groups(),
-      Profile()
+      Profile(id: id, token: token)
     ];
 
     return Scaffold(
